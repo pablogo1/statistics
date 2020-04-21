037267ea9b7f54f79f3507dc4d4d121926e66c54
Pablo Gonzalez <pablogr.gonzalez@gmail.com>
2020-04-21 00:53:26 -0500

    normal distribution II complete

--- normal-dist/normal_distribution.py
+++ normal-dist/normal_distribution.py
@@ -6,3 +6,8 @@ def normal_cdf(x, mean=0, std_dev=1) -> float:
     '''
     return 0.5 * (1 + math.erf((x - mean) / (std_dev * math.sqrt(2))))
 
+def normal_pdf(x, mean, std_dev):
+    ''' Returns the normal PDF (probability density function) of normal distribution
+        given the mean and standard deviation.
+    '''
+    return math.exp(-0.5 * ((x - mean) / std_dev)**2) / (std_dev * math.sqrt(2 * math.pi))
--- normal-dist/problem2.py
+++ normal-dist/problem2.py
@@ -1,10 +1,15 @@
 from normal_distribution import normal_cdf
 
 class Problem2:
-    mean = 20.0
-    std_dev = 2.0
+    mean = 70.0
+    std_dev = 10.0
 
     def run(self, param1, param2):
-        pass
+        pct_larger_than_80 = (1 - normal_cdf(80, self.mean, self.std_dev)) * 100
+        pct_larger_equal_than_60 = (1 - normal_cdf(60, self.mean, self.std_dev)) * 100
+        pct_less_than_60 = normal_cdf(60, self.mean, self.std_dev) * 100
+        print (f"{pct_larger_than_80:.2f}")
+        print (f"{pct_larger_equal_than_60:.2f}")
+        print (f"{pct_less_than_60:.2f}")
         # p_less_than_p2 = normal_cdf(param2, self.mean, self.std_dev) - normal_cdf(param1, self.mean, self.std_dev)
         # return p_less_than_p2
